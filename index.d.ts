2624da49bda3372e7a5997f42993fa7f0174cbf3
Anthony De Leon <tonton.dev001@gmail.com>
2016-08-19 17:54:48 +0800

    publish

---
+++ index.d.ts
@@ -0,0 +1,77 @@
+import * as events from 'events';
+/**
+ * Standard object interface
+ */
+export interface StdObject extends Object {
+    [key: string]: any;
+}
+export declare class Spidey extends events.EventEmitter {
+    private host;
+    /**
+     * Algorithm for the crypto cipher
+     * @type {string}
+     */
+    protected static algo: string;
+    /**
+     * Data Connection Mapping
+     * @type {Map<string, WebCrawler>}
+     */
+    private static dataClientMap;
+    constructor(host: string);
+    /**
+     * Get an access token for your app
+     * @param {string} key
+     * @param {string} secret
+     * @returns {Promise<StdObject>}
+     */
+    getToken(key: string, secret: string): Promise<StdObject>;
+    /**
+     * Create DataClient instance
+     * @param {string} token
+     * @param {number} crawlerId
+     * @param {number} parserId
+     * @param {string} name
+     * @returns {DataClient|null}
+     */
+    createDataConnection(token: string, crawlerId: number, parserId: number, name?: string): DataClient;
+    /**
+     * Get DataClient by name
+     * @param  {string} name
+     * @return {DateClient}
+     */
+    getDataClient(name: string): DataClient;
+}
+/**
+ * DataClient class
+ */
+export declare class DataClient extends events.EventEmitter {
+    private host;
+    private token;
+    private crawlerId;
+    private parserId;
+    /**
+     * Default Url for Data endpoint
+     */
+    private url;
+    constructor(host: string, token: string, crawlerId: number, parserId: number);
+    /**
+     * Get crawled data infinitely
+     * @param {string} token
+     * @param {number} crawlerId
+     * @param {number} parserId
+     * @param {?}url
+     */
+    fetch(url?: string): Promise<void>;
+    /**
+     * Listen on fetch complete event
+     * @param  {(StdObject) => void} fn
+     * @return {this}
+     */
+    onFetchComplete(fn: (data: StdObject) => void): this;
+    /**
+     * Listen on error event
+     * @param  {(err) => void} fn
+     * @return {this}
+     */
+    onError(fn: (err) => void): this;
+}
---
+++ index.js
@@ -0,0 +1,169 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
+const events = require('events');
+const crypto = require('crypto');
+const requestLib = require('request');
+class HttpClient {
+    /**
+     * Make a http request
+     * @param  {string} method
+     * @param  {string} url
+     * @param  {HttpClientOptions} opts
+     * @return {Promise<HttpClientResponse>}
+     */
+    static call(method, url, opts) {
+        return new Promise((resolve, reject) => {
+            requestLib(url, HttpClient.merge({}, opts, { method: method.toUpperCase() }), (err, response, body) => {
+                if (err)
+                    return reject(err);
+                resolve({ response, body });
+            });
+        });
+    }
+    /**
+     * Merge all object properties
+     * @param  {StdObject} target
+     * @param  {StdObject[]} ...args
+     * @return {StdObject}
+     */
+    static merge(target, ...args) {
+        var sources = [].slice.call(arguments, 1);
+        sources.forEach(function (source) {
+            for (var prop in source) {
+                target[prop] = source[prop];
+            }
+        });
+        return target;
+    }
+}
+class Spidey extends events.EventEmitter {
+    constructor(host) {
+        super();
+        this.host = host;
+    }
+    /**
+     * Get an access token for your app
+     * @param {string} key
+     * @param {string} secret
+     * @returns {Promise<StdObject>}
+     */
+    getToken(key, secret) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let date = new Date();
+            let cipher = crypto.createCipher(Spidey.algo, secret);
+            let text = JSON.stringify({ key: key, time: date.getTime() });
+            let crypted = cipher.update(text, 'utf8', 'hex');
+            crypted += cipher.final('hex');
+            let options = {
+                headers: {
+                    'key': key,
+                    'hash': crypted
+                }
+            };
+            let response = yield HttpClient.call('GET', this.host + '/access', options);
+            return JSON.parse(response['body']);
+        });
+    }
+    /**
+     * Create DataClient instance
+     * @param {string} token
+     * @param {number} crawlerId
+     * @param {number} parserId
+     * @param {string} name
+     * @returns {DataClient|null}
+     */
+    createDataConnection(token, crawlerId, parserId, name) {
+        name = (name ? name : (crawlerId + parserId));
+        if (!Spidey.dataClientMap.has(name)) {
+            Spidey.dataClientMap.set(name, new DataClient(this.host, token, crawlerId, parserId));
+        }
+        return Spidey.dataClientMap.get(name) || null;
+    }
+    /**
+     * Get DataClient by name
+     * @param  {string} name
+     * @return {DateClient}
+     */
+    getDataClient(name) {
+        return Spidey.dataClientMap.get(name) || null;
+    }
+}
+/**
+ * Algorithm for the crypto cipher
+ * @type {string}
+ */
+Spidey.algo = 'aes-256-ctr';
+/**
+ * Data Connection Mapping
+ * @type {Map<string, WebCrawler>}
+ */
+Spidey.dataClientMap = new Map();
+exports.Spidey = Spidey;
+/**
+ * DataClient class
+ */
+class DataClient extends events.EventEmitter {
+    constructor(host, token, crawlerId, parserId) {
+        super();
+        this.host = host;
+        this.token = token;
+        this.crawlerId = crawlerId;
+        this.parserId = parserId;
+        this.url = host + '/crawlers/' + crawlerId + '/parsers/' + parserId + '/data';
+    }
+    /**
+     * Get crawled data infinitely
+     * @param {string} token
+     * @param {number} crawlerId
+     * @param {number} parserId
+     * @param {?}url
+     */
+    fetch(url) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let options = { headers: { 'token': this.token } };
+            if (!url) {
+                url = this.url;
+            }
+            let response = yield HttpClient.call('GET', url, options);
+            let body = JSON.parse(response['body']);
+            if (body.error) {
+                this.emit('error', body);
+            }
+            if (body.results.length) {
+                this.emit('fetchComplete', body.results);
+            }
+            // sleep
+            yield new Promise((resolve) => {
+                setTimeout(resolve, 1000);
+            });
+            this.fetch(body.meta.next);
+        });
+    }
+    /**
+     * Listen on fetch complete event
+     * @param  {(StdObject) => void} fn
+     * @return {this}
+     */
+    onFetchComplete(fn) {
+        this.on('fetchComplete', fn);
+        return this;
+    }
+    /**
+     * Listen on error event
+     * @param  {(err) => void} fn
+     * @return {this}
+     */
+    onError(fn) {
+        this.on('error', fn);
+        return this;
+    }
+}
+exports.DataClient = DataClient;
+//# sourceMappingURL=index.js.map
